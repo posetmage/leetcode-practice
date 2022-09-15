c2db7653687762354126103c690fb9b9b31e151d
PosetMage <PosetMage@gmail.com>
2022-09-15 17:46:11 +0800

    all

---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.email "PosetMage@gmail.com"
+git config --local user.name "PosetMage"
+git remote set-url origin git@POM:posetmage/leetcode-practice.git
+
+git pull
+git add .
+git commit -m "all"
+git push
